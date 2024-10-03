42d4ecf5e2b59160949c5fc486610477c11fee44
hamzasand <mhamzasaeed762@gmail.com>
2024-10-03 16:14:15 +0500

    Updated_version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_chat_Bot',
+    version= '0.0.0',
+    author= 'Muhammad Hamzaq',
+    author_email= 'mhamza7197@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
